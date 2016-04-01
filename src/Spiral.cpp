24b03d621140cc1d8f660ccb9875ede57ee1e1c0
kavya9 <kavya9@missionrnd.com>
2016-04-01 12:37:07 +0530

    Submit Commit

--- src/Spiral.cpp
+++ src/Spiral.cpp
@@ -33,8 +33,58 @@ Note : Check the function Parameters ,Its a double pointer .
 
 #include "stdafx.h"
 #include<stdlib.h>
+int *spiralRecursion(int top, int bottom, int left, int right, int direction, int ** input_array, int * result, int resIndex, int eles)
+{
+	int i;
+	if (eles == resIndex) return result;
 
+	if (direction == 0)
+	{
+		for (i = left; i <= right; i++)
+		{
+			result[resIndex] = input_array[top][i];
+			resIndex++;
+		}
+		top++;
+	}
+	else if (direction == 1)
+	{
+		for (i = top; i <= bottom; i++)
+		{
+			result[resIndex] = input_array[i][right];
+			resIndex++;
+		}
+		right--;
+
+	}
+	else if (direction == 2)
+	{
+		for (i = right; i >= left; i--)
+		{
+			result[resIndex] = input_array[bottom][i];
+			resIndex++;
+		}
+		bottom--;
+
+	}
+	else if (direction == 3)
+	{
+		for (i = bottom; i >= top; i--)
+		{
+			result[resIndex] = input_array[i][left];
+			//printf("%d ", result[resIndex]);
+			resIndex++;
+		}
+		left++;
+
+	}
+	direction = (direction + 1) % 4;
+	return spiralRecursion(top, bottom, left, right, direction, input_array, result, resIndex, eles);
+}
 int *spiral(int rows, int columns, int **input_array)
 {
-	return NULL;
+	int top = 0, bottom = rows - 1, left = 0, right = columns - 1, direction = 0,resIndex=0;
+	int *result = (int *)malloc(rows*columns*sizeof(int));
+	if(rows<=0 || columns <=0 ||input_array==NULL)return NULL;
+	return spiralRecursion(top, bottom, left, right, direction,input_array, result, resIndex,rows*columns);
 }
